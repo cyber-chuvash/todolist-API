c83a6d5405299b13026d7b26d01c25a7c59db0bb
Mikhail Migushov <m.migushov@gmail.com>
2020-06-30 14:32:57 +0300

    Use scoped session for Flask

--- app/db.py
+++ app/db.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from app.models.base import Base
 
 engine = create_engine('sqlite:///:memory:', echo=True)
-Session = sessionmaker(bind=engine)
+Session = scoped_session(sessionmaker(bind=engine))
 
 Base.metadata.create_all(engine)
