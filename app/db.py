58e633e1b981396f95c5909f8699d3a69c3b3c9d
Mikhail Migushov <m.migushov@gmail.com>
2020-06-30 16:34:52 +0300

    Declare models before use

--- app/db.py
+++ app/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from app.models.base import Base
+from app.models import Base
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 Session = scoped_session(sessionmaker(bind=engine))
