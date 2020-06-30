ad2403066ea7412418a4e2d08972f37d877093f4
Mikhail Migushov <m.migushov@gmail.com>
2020-06-30 18:49:06 +0300

    Use in-file sqlite db (in-memory doesn't work with flask)

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 from app.models import Base
 
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('sqlite:///tmp/db.sqlite', echo=True)
 Session = scoped_session(sessionmaker(bind=engine))
 
 Base.metadata.create_all(engine)
