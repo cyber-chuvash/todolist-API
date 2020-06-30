5aa135279fa15f09c391c7343474b020b4b41395
Mikhail Migushov <m.migushov@gmail.com>
2020-06-30 13:23:18 +0300

    Add docstring for base_repr()

--- models/base.py
+++ models/base.py
@@ -2,6 +2,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 def base_repr(self):
+    """
+    Creates beautiful repr() strings for an object based on it's classname and __dict__
+    Made specifically for objects that inherit from SQLAlchemy's declarative base, but will probably work for others too
+
+    :param self: Object, instance of SQLAlchemy's declarative base
+    :return: str: repr string, that looks like this: "<ClassName(attr1='value', attr2=321)>"
+    """
+
     # Filter for only public attributes
     attrs = filter(lambda t: not t[0].startswith('_'), self.__dict__.items())
     # Exclude instances of the same base class to avoid circular recursion with one-to-many relationships
