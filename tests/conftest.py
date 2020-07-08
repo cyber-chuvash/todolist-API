a1277eef9be940ea190040902b54edc428a3a91d
Mikhail Migushov <m.migushov@gmail.com>
2020-07-08 22:17:09 +0300

    Add tests for GET and DELETE /account/

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 import pytest
 
 import app
@@ -16,3 +19,45 @@ def flask_app(monkeypatch):
 @pytest.fixture
 def flask_client(flask_app):
     yield flask_app.create_app().test_client()
+
+
+@pytest.fixture
+def user_account(flask_client):
+    class UserAccount:
+        def __init__(self, username):
+            self.username = username
+
+            self.is_registered = False
+            self.id = None
+
+        @classmethod
+        def create_random(cls):
+            username = ''.join(random.choices(string.ascii_lowercase, k=8))
+            return cls(username)
+
+        @property
+        def email(self):
+            return f'{self.username}@gmail.com'
+
+        @property
+        def reg_repr(self):
+            return {
+                "username": self.username,
+                "email": self.email
+            }
+
+        def register(self):
+            if self.is_registered:
+                raise ValueError('Account already registered')
+            res = flask_client.post('/account/', json=self.reg_repr)
+            self.id = res.get_json()['id']
+            self.is_registered = True
+
+        @property
+        def auth_header(self):
+            return 'Authorization', f'Bearer {self.id}'
+
+    acc = UserAccount.create_random()
+    acc.register()
+
+    return acc
--- tests/test_account_routes.py
+++ tests/test_account_routes.py
@@ -1,4 +1,9 @@
 
+
+####################
+#  POST /account/  #
+####################
+
 def test_create_account(flask_client):
     shrek = {
         "username": "Shrek",
@@ -83,3 +88,64 @@ def test_create_account_existing_email(flask_client):
     assert "error" in err_data
     assert "email" in err_data['error']
     assert "exists" in err_data['error']
+
+
+######################
+#   GET /account/    #
+######################
+
+def test_get_account(flask_client, user_account):
+    res = flask_client.get('/account/', headers=[user_account.auth_header])
+    assert res.status_code == 200
+
+    data = res.get_json()
+    assert data is not None
+    assert "error" not in data
+
+    assert data.get('id') == user_account.id
+    assert data.get('email') == user_account.email
+    assert data.get('username') == user_account.username
+
+
+def test_get_nonexistent_account(flask_client):
+    res = flask_client.get('/account/', headers=[('Authorization', 'Bearer 1337322696969')])
+    assert res.status_code == 404
+
+    data = res.get_json()
+    assert "error" in data
+    assert "account" in data['error'].lower()
+    assert "not found" in data['error']
+
+
+def test_get_account_no_auth(flask_client):
+    # Basically check that we didn't forget to add @auth_required
+    # Headers, error msg, etc. are covered by test_auth tests
+    res = flask_client.get('/account/')
+    assert res.status_code == 401
+
+
+######################
+#  DELETE /account/  #
+######################
+
+def test_delete_account(flask_client, user_account):
+    res = flask_client.delete('/account/', headers=[user_account.auth_header])
+    assert res.status_code == 200
+
+    get_deleted_user_res = flask_client.get(f'/users/{user_account.id}')
+    assert get_deleted_user_res.status_code == 404
+
+
+def test_repeated_delete_account(flask_client, user_account):
+    res = flask_client.delete('/account/', headers=[user_account.auth_header])
+    assert res.status_code == 200
+
+    del_again_res = flask_client.delete('/account/', headers=[user_account.auth_header])
+    assert del_again_res.status_code == 404
+    assert "error" in del_again_res.get_json()
+    assert "already deleted" in del_again_res.get_json()['error']
+
+
+def test_delete_account_no_auth(flask_client):
+    res = flask_client.delete('/account/')
+    assert res.status_code == 401
